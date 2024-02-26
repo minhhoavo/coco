5f68e82a5c019716761b3f0d027cfb6ee9e335e7
hoavo1911 <vominhhoa19112000.dx.bp@gmail.com>
2024-02-26 16:44:25 +0700

    upload source

---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$('.home-design__slidercontainer').slick({
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+    prevArrow: $(".home-design__btnprev"),
+    nextArrow: $(".home-design__btnnext"),
+  });
+
